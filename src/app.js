dbdf7ffa0e4b529150cab1cba442e41fa638c160
steven.cardona <steven.cardona@globant.com>
2022-04-19 11:36:04 -0500

    Resolve problem about import/export modules

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-export const sum = (a, b) => {
+const sum = (a, b) => {
   return a + b;
 };
 
-export const convertName = (nombre) => {
+const convertName = (nombre) => {
   return nombre.toUpperCase;
 };
 
-export const validateEmail = (email) => email.includes("@gmail.com");
+const validateEmail = (email) => email.includes("@gmail.com");
 
-export const Person = {
+const Person = {
   name: "Steven",
   age: 18,
   showInfo: function () {
     return this.name + " " + this.age;
   },
 };
+
+module.exports = { validateEmail, Person, sum, convertName };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { validateEmail, convertName, sum, Person } from "./src/app";
+const { validateEmail, sum, convertName, Person } = require("./app");
 
 const res = validateEmail("steven.cardona@gmail.com");
 
@@ -7,8 +7,6 @@ const result = sum(2, 4);
 const newName = convertName("steven");
 
 const showInfoPerson = Person.showInfo;
-
-console.log(showInfoPerson);
 
 console.assert(result == 6, "Faile Test Sum(2,4) - Expect 6 | ToBe 6");
 console.assert(sum(2, 1) == 3, "Faile Test Sum(2,1) - Expect 3 | ToBe 3");
