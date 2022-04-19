263898b77747c81921b6b21719025592584d7ab0
steven.cardona <steven.cardona@globant.com>
2022-04-19 11:10:55 -0500

    Feature: Creating test for sum function

--- main.js
+++ main.js
@@ -9,3 +9,6 @@ const newName = convertName("steven");
 const showInfoPerson = Person.showInfo;
 
 console.log(showInfoPerson);
+
+console.assert(result == 6, "Faile Test Sum(2,4) - Expect 6 | ToBe 6");
+console.assert(sum(2, 1) == 3, "Faile Test Sum(2,1) - Expect 3 | ToBe 3");
